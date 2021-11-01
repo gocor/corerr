40c092e7c0f673c5ca700df4d89cb6d21d339bb8
Bilal Khan <bkhan@bkhan.com>
2021-11-01 00:34:25 -0700

    Added Wrapped http errors

--- error.go
+++ error.go
@@ -5,7 +5,17 @@ func NewStdError(code int) error {
 	return NewCoreError(code, errMsgMap[code])
 }
 
+// NewWrappedStdError ...
+func NewWrappedStdError(code int, err error) error {
+	return NewWrappedCoreError(code, errMsgMap[code], err)
+}
+
 // NewHTTPStatusError ...
 func NewHTTPStatusError(httpStatusCode int) error {
 	return NewStdError(10000 + httpStatusCode)
+}
+
+// NewWrappedHTTPStatusError ...
+func NewWrappedHTTPStatusError(httpStatusCode int, err error) error {
+	return NewWrappedStdError(10000+httpStatusCode, err)
 }
